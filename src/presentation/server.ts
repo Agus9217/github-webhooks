f53da593127faa1e6ffdf5f8a030be8cb9ce0f55
Agus9217 <justinibarrola92@gmail.com>
2024-01-16 14:24:12 -0300

    creando controller github

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { GithubController } from "./github/controller";
 
 interface Opt {
   port: number;
@@ -7,15 +8,16 @@ interface Opt {
 export class Server {
   private readonly app = express();
   private port: number;
+  private controller = new GithubController();
 
   constructor({ port }: Opt) {
     this.port = port;
   }
 
   start() {
-    this.app.post("/api/github", (req, res) => {
-      res.send('Hola desde github url')
-    });
+    this.app.use(express.json());
+
+    this.app.post("/api/github", this.controller.webhookHandler);
 
     this.app.listen(this.port, () => {
       console.log(`Starting server on: http://localhost:${this.port}`);
